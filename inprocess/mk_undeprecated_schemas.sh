c7d516f5ab0e1b0cb3b5263771671ed86816985a
tcatapano <thc4ster@gmail.com>
2017-09-06 17:19:14 -0700

    modify mk_undeprecated_schemas.sh; regenerate

--- inprocess/mk_undeprecated_schemas.sh
+++ inprocess/mk_undeprecated_schemas.sh
@@ -5,13 +5,13 @@ DTD_FLATTEN="dtdflatten/dtd-flatten-3.2.9.jar"
 
 $INCELIM -saxon ead3_undeprecated-driver.rng
 
-mv ead3_undeprecated-driver-compiled.rng ead3_undeprecated.rng 
+mv ead3_undeprecated-driver-compiled.rng ../undeprecated/ead3_undeprecated.rng 
 
 
 $INCELIM -saxon ead3_undeprecated_dtd.rng
 
 java -jar  $TRANG ead3_undeprecated_dtd-compiled.rng  ead3_undeprecated_dtd-compiled.dtd
 
-java -jar  $DTD_FLATTEN ead3_undeprecated_dtd-compiled.dtd > ead3_undeprecated.dtd
+java -jar  $DTD_FLATTEN ead3_undeprecated_dtd-compiled.dtd > ../undeprecated/ead3_undeprecated.dtd
 
 #xmlstarlet sel -t -v //h1 -v //*[@class = contentdesc] elem-*.html  | sed 's/&gt;/> /' | sed 's/&lt;/</'
